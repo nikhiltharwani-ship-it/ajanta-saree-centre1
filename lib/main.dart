cc5d6ff3d49579aa8a15fe672b9f808062bd387f
nikhiltharwani-ship-it <nikhil.tharwani@gmail.com>
2026-08-31 23:26:16 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,13 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
   runApp(const AjantaApp());
 }
 
