e2ca3e1d6aa1a570dbd389ce1b4d9759454e9768
nikhiltharwani-ship-it <nikhil.tharwani@gmail.com>
2026-09-03 23:13:16 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1135,19 +1135,18 @@ class PaymentStorage {
   // DELETE ONE PAYMENT
   // ==========================================================
 
-    static Future<void> delete(
+      static Future<void> delete(
     String paymentId,
   ) async {
     final payments = await load();
 
-    String affectedCustomerId = '';
+    payments.removeWhere(
+      (payment) => payment.id == paymentId,
+    );
 
-    for (final payment in payments) {
-      if (payment.id == paymentId) {
-        affectedCustomerId = payment.customerId;
-        break;
-      }
-    }
+        await save(payments);
+  }
+}
 
     payments.removeWhere(
       (payment) => payment.id == paymentId,
